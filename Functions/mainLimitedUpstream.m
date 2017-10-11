b485a61c20818f6eda65b21c99bccb2c6ccd81b7
Matthew Kemnetz <mkemnetz@nd.edu>
2017-10-11 01:29:13 -0400

    adding stub comments

--- Functions/mainLimitedUpstream.m
+++ Functions/mainLimitedUpstream.m
@@ -1,7 +1,50 @@
-function [ output_args ] = mainLimitedUpstream( input_args )
-%MAINLIMITEDUPSTREAM Summary of this function goes here
-%   Detailed explanation goes here
+function [output1,output2] = mainLimited(input1,input2,input3) 
+%MAINLIMITED - One line description of what the function or script performs (H1 line) 
+%Optional file header info (to give more details about the function than in the H1 line) 
+%Optional file header info (to give more details about the function than in the H1 line) 
+%Optional file header info (to give more details about the function than in the H1 line) 
+% 
+% Syntax:  [output1,output2] = mainLimited(input1,input2,input3) 
+% 
+% Inputs: 
+%    input1 - Description 
+%    input2 - Description 
+%    input3 - Description 
+% 
+% Outputs: 
+%    output1 - Description 
+%    output2 - Description 
+% 
+% Example: 
+%    Line 1 of example 
+%    Line 2 of example 
+%    Line 3 of example 
+% 
+% Other m-files required: none 
+% Subfunctions: none 
+% MAT-files required: none 
+% 
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2 
+ 
+% Author: Matthew Kemnetz 
+% Hessert Laboratory for Aerospace Research B034 
+% email: mkemnetz@nd.edu, kemnetz.m@gmail.com 
+% Website: http://www.matthewkemnetz.com 
+% October 2017; Last revision: 11-October-2017 
+% Copyright 2017, Matthew Kemnetz, All rights reserved. 
+ 
+%% ------------- BEGIN CODE -------------- %% 
+ 
+Enter your executable matlab commands here 
 
 
-end
+
+
+
+
+
+
+
+
+ % ===== EOF ====== [mainLimited.m] ======  
 
